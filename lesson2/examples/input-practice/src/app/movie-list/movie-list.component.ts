69e3d7b72f3e4a73f58186d0cc45cf03f9ee02fa
MeenaKarthik <meenafresh@gmail.com>
2026-01-28 15:16:51 -0600

    add movie from webpage (display)

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieListComponent implements OnInit {
    movies = ['Toy Story', 'The Shining', 'Sleepless in Seattle', 'The Martian'];
+   errMessage: string = '';
 
    constructor() { }
 
    ngOnInit() {
+   }
+
+   addMovie(newTitle: string){
+      const title = newTitle.trim();
+      if(title === ''){
+      this.errMessage = 'Empty string! Provide a movie name!';
+      }
+      else if (this.movies.some(m => m.toLowerCase() === title.toLowerCase()))
+      {
+        this.errMessage = "Duplicate entry exists! Provide another movie name!"; 
+      }
+      else {       
+         this.movies.push(title);
+         this.errMessage = ''; 
+      }     
    }
 }
